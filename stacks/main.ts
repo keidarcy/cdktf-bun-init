54b74047cc1ed518a70eba55b40b7f572b3018c4
Xing Yahao <keidarcy.1015@gmail.com>
2024-10-28 22:27:56 +0900

    format and ci

--- stacks/main.ts
+++ stacks/main.ts
@@ -5,15 +5,15 @@ import type { Construct } from "constructs";
 const app = new App();
 
 class CdktfStack extends TerraformStack {
-	constructor(scope: Construct, id: string) {
-		super(scope, id);
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
 
-		new provider.AwsProvider(this, "aws", {});
+    new provider.AwsProvider(this, "aws", {});
 
-		new s3Bucket.S3Bucket(this, "test-bucket", {
-			bucket: "keidarcy-cdktf-bun-init-test-bucket-name",
-		});
-	}
+    new s3Bucket.S3Bucket(this, "test-bucket", {
+      bucket: "keidarcy-cdktf-bun-init-test-bucket-name",
+    });
+  }
 }
 new CdktfStack(app, "cdktf-stack");
 
